21fcb26a796fd9cbec4be930af256cadcd04977e
Lenodeoliveira <lenodeoliveira@gmail.com>
2023-01-02 11:01:46 -0300

    feat: adds function to unregister an event

--- ddd-patterns/src/domain/event/@shared/event-dispatcher.spec.ts
+++ ddd-patterns/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -12,4 +12,19 @@ describe('Domain Events tests', () => {
         expect(eventDispatcher.getEventHanlers["ProductCreatedEvent"].length).toBe(1);
         expect(eventDispatcher.getEventHanlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler)
     })
+
+    test('Should unregister an event handler', () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHanlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHanlers["ProductCreatedEvent"].length).toBe(1);
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler)
+
+        expect(eventDispatcher.getEventHanlers["ProductCreatedEvent"].length).toBe(0)
+    
+    })
 })
